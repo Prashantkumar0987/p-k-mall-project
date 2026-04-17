bc7c95d90fd468d682028130cd4c828688779a84
Prashant kumar <prashant8252241631@gmail.com>
2026-04-17 18:50:58 +0530

    Update product fetch URL to production server

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   // 🔄 Fetch products
   useEffect(() => {
-    fetch("http://localhost:5001/products")
+    fetch("https://p-k-mall-project-1.onrender.com")
       .then((res) => res.json())
       .then((data) => {
         setProducts(Array.isArray(data) ? data : []);
